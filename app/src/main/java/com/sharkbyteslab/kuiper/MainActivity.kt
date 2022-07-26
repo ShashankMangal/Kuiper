c37146f1cd88fc00b151b1e2dee9bbee69ac2b02
ShashankMangal <80992045+ShashankMangal@users.noreply.github.com>
2022-07-26 14:12:45 +0530

    fragments added

--- app/src/main/java/com/sharkbyteslab/kuiper/MainActivity.kt
+++ app/src/main/java/com/sharkbyteslab/kuiper/MainActivity.kt
@@ -2,12 +2,32 @@ package com.sharkbyteslab.kuiper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.sharkbyteslab.kuiper.Adapters.ViewPagerAdapter
+import com.sharkbyteslab.kuiper.Fragments.Call
+import com.sharkbyteslab.kuiper.Fragments.Chat
+import com.sharkbyteslab.kuiper.Fragments.Status
+import com.sharkbyteslab.kuiper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
 
+        val fragmentArrayList = ArrayList<Fragment>()
+
+        fragmentArrayList.add(Chat())
+        fragmentArrayList.add(Status())
+        fragmentArrayList.add(Call())
+
+        val adapter = ViewPagerAdapter(this, supportFragmentManager, fragmentArrayList)
+
+        binding!!.viewPager.adapter = adapter
+        binding!!.tabs.setupWithViewPager(binding!!.viewPager)
 
 
     }
